143003a919945e1405efc69cfcf0a5f350426616
Leo Vu <leo@politetech.com>
2023-10-10 11:59:57 +0700

    Add frontend code

---
+++ frontend/src/common/utils/detect.ts
@@ -0,0 +1,2 @@
+export const isAndroid = () => navigator.userAgent.match(/Android/i) !== null;
+export const isIos = () => navigator.userAgent.match(/iPhone|iPad|iPod/i) !== null;
---
+++ frontend/src/test-utils.tsx
@@ -0,0 +1,20 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React, { ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+};
+
+const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from '@testing-library/react';
+export { customRender as render };
